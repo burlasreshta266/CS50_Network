55c6da32cf2a01ebbfe3b33a67c0b6bcc872c5ac
Sreshta <burlasreshta26@gmail.com>
2026-01-04 20:18:59 +0530

    all posts and create new post

--- network/views.py
+++ network/views.py
@@ -1,14 +1,29 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse
-
-from .models import User
+from .models import User, Post, Follow, Like
+from .forms import createPostForm
 
 
 def index(request):
-    return render(request, "network/index.html")
+    all_posts = Post.objects.all().order_by('-timestamp')
+
+    if request.method=='POST':
+        form = createPostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+        else:
+            form = createPostForm()
+        return redirect('index')
+    else:
+        return render(request, "network/index.html", {
+            'all_posts' : all_posts,
+            'postForm' : createPostForm()
+        })
 
 
 def login_view(request):
